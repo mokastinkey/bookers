84c98bda12fd06b9c43fe2ab78b2911226a82ad6
mokastinkey <mocastinkey@gmail.com>
2021-07-03 04:35:24 +0000

    [bookers]createコントローラ修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,17 @@
 class BooksController < ApplicationController
-  
+
   def index
     @books = Book.all
     @book = Book.new
   end
-  
+
   def create
-    @books = Book.all
-    @book = Book.new(book_params)
-    if @book.save
-      redirect_to book_path(@book.id), notice: 'Book was successfully created.'
+    book = Book.new(book_params)
+    if book.save
+      redirect_to book_path(book.id), notice: 'Book was successfully created.'
     else
+      @book = book
+      @books = Book.all
       render :index
     end
   end
@@ -22,7 +23,7 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
@@ -30,20 +31,20 @@ class BooksController < ApplicationController
     else
       render :edit
     end
-  end 
-  
+  end
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
     redirect_to books_path
   end
-  
-  
-  
+
+
+
   private
   #ストロングパラメータ
   def book_params
     params.require(:book).permit(:title, :body)
   end
-    
+
 end
